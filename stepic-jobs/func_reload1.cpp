c06daf665160555d197c4a71037785f88bd41d8d
lsemus <96842171+lsemus@users.noreply.github.com>
2022-05-19 10:41:09 +0300

    перегрузка функций

--- stepic-jobs/func_reload1.cpp
+++ stepic-jobs/func_reload1.cpp
@@ -4,12 +4,16 @@ void foo(char) { std::cout << "char" << std::endl; }
 void foo(signed char) { std::cout << "signed char" << std::endl; }
 void foo(unsigned char) { std::cout << "unsigned char" << std::endl; }
 
+float  square(float value) { return value * value; }
+// double square(float value) { return (double)value * value; }
 
 int main()
 {
 
-foo('a'); // char
-//foo(97); //ошибка компиляции
+foo('a');
+// foo(97); // error: call of overloaded ‘foo(int)’ is ambiguous
+
+// double sq = square(2.0); //error ambiguating new declaration of ‘double square(float)’
 
 return 1;
 }
